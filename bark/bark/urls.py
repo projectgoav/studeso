085651234a48ef6213954bb4d082df0ef671270f
projectgoav <2025797M@student.gla.ac.uk>
2015-03-03 13:27:14 +0000

    Added basic url structure based on the proposed url file

--- bark/bark/urls.py
+++ bark/bark/urls.py
@@ -10,11 +10,75 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
+    	#PROJECT URLS
+    	#----------
 
-		#Main Application URLs
+		#Main Application Root
         url(r'^$', views.index, name='index'),
 
-        #Registration Views
-        url(r'signup/', regviews.index, name='regIndex')
+        #Just redirect back to homepage?
+        #TODO
+        url(r'^bark/' views.index, name='Barkindex'),
+
+		#BARK URLS
+		#----------
+
+        #Generic Post list
+        #TODO
+        #Add tags as /TAG/TAG/TAG to filter depending on the tags
+        url(r'^bark/tags/', views.posts, name='posts'),
+
+        #Specific Bark view
+        url(r'^bark/<post_id>/', views.postview, name ='postview'),
+       
+        #Add new Bark
+        url(r'^bark/new/', views.addbark, name='addbark'),
+
+        #Search
+        #Followed by query string or some form of search regex
+        url(r'^search/', views.search, name='search'),
+
+
+        #UUSER PROFILE FILES
+        #---------------
+
+        #Displays list of users
+        #Ranked by helpfulness?
+        #TODO
+        url(r'^users/', regviews.users, name='users'),
+
+        #Displays specific user profile
+        url(r'^users/<user_name>/', regviews.profile, name='profile'),
+
+        #PERSONAL PROFILE VIEWS
+        #------------
+
+        #View your own profile, when logged in
+        url(r'^profile/', regviews.profile, name="profile"),
+
+        #Update your profile information
+        url(r'^profile/update/', regviews.profileUpdate, name='profileUpdate'),
+
+
+        #REGISTRATION VIEWS
+        #---------
+
+        #Register
+        url(r'^signup/', regviews.index, name='signup'),
+
+        #Login
+        url(r'^signin/', regviews.signin, name = 'signin'),
+
+        #Logout
+        url(r'^signout/', regviews.signout, name='signout'),
+
+        #Display password options
+        url(r'^password/', regviews.passwordMenu, name = 'passwordMenu'),
+
+        #Change password
+        url(r'^password/change/', regviews.passwordChange, name = 'passwordChange'),
+        
+        #Reset password
+        url(r'^password/reset/', regviews.passwordReset, name= 'passwordReset'),
 
 )
