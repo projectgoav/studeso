6d58b27a09a71c3adaaffcaee36753576066e126
Michael McKay <michael_mac_i@me.com>
2015-03-04 20:35:00 +0000

    Little tidy

--- bark/bark/urls.py
+++ bark/bark/urls.py
@@ -15,29 +15,29 @@ urlpatterns = patterns('',
     	#----------
 
 		#Main Application Root
-        url(r'^$', views.index, name='index'),
+        url(r'^$', views.index, name = 'index'),
 
         #Just redirect back to homepage?
         #TODO
-        url(r'^bark/' views.index, name='Barkindex'),
+        url(r'^bark/' views.index, name = 'barkIndex'),
 
 		#BARK URLS
 		#----------
 
         #Generic Post list
-        #TODO
-        #Add tags as /TAG/TAG/TAG to filter depending on the tags
-        url(r'^bark/tags/', views.barks, name='barks'),
+        # TODO
+        # Add tags as /TAG/TAG/TAG to filter depending on the tags
+        url(r'^bark/tags/', views.barks, name = 'barks'),
 
         #Specific Bark view
-        url(r'^bark/<post_id>/', views.barkview, name ='barkview'),
+        url(r'^bark/<post_id>/', views.barkview, name = 'barkview'),
        
         #Add new Bark
-        url(r'^bark/new/', views.addbark, name='addbark'),
+        url(r'^bark/new/', views.addbark, name = 'addbark'),
 
         #Search
         #Followed by query string or some form of search regex
-        url(r'^search/', views.search, name='search'),
+        url(r'^search/', views.search, name = 'search'),
 
 
         #UUSER PROFILE FILES
@@ -46,51 +46,37 @@ urlpatterns = patterns('',
         #Displays list of users
         #Ranked by helpfulness?
         #TODO
-        url(r'^users/', regviews.users, name='users'),
+        url(r'^users/', regviews.users, name = 'users'),
 
         #Displays specific user profile
-        url(r'^users/<user_name>/', regviews.viewuser, name='viewuser'),
+        url(r'^users/<user_name>/', regviews.viewuser, name = 'viewuser'),
 
         #PERSONAL PROFILE VIEWS
         #------------
 
         #View your own profile, when logged in
-        url(r'^profile/', regviews.profile, name="profile"),
+        url(r'^profile/', regviews.profile, name = "profile"),
 
         #Update your profile information
-        url(r'^profile/update/', regviews.profileUpdate, name='profileUpdate'),
+        url(r'^profile/update/', regviews.profileUpdate, name = 'profileUpdate'),
 
 
         #REGISTRATION VIEWS
         #---------
 
-        #Register
-        url(r'^signup/', regviews.signup, name='signup'),
-
-        #Login
+        url(r'^signup/', regviews.signup, name = 'signup'),
         url(r'^signin/', regviews.signin, name = 'signin'),
+        url(r'^signout/', regviews.signout, name = 'signout'),
 
-        #Logout
-        url(r'^signout/', regviews.signout, name='signout'),
-
-        #Display password options
         url(r'^password/', regviews.passwordMenu, name = 'passwordMenu'),
-
-        #Change password
         url(r'^password/change/', regviews.passwordChange, name = 'passwordChange'),
-        
-        #Reset password
-        url(r'^password/reset/', regviews.passwordReset, name= 'passwordReset'),
-
+        url(r'^password/reset/', regviews.passwordReset, name = 'passwordReset'),
 )
 
-
-#Temp bit fo uploading profile images during developement
+# Temp bit fo uploading profile images during developement
 if settings.DEBUG:
     urlpatterns += patterns(
         'django.views.static',
         (r'^media/(?P<path>.*)',
         'serve',
         {'document_root': settings.MEDIA_ROOT}), 
-
-
