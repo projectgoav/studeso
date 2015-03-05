d2820524308615c275270d28813f9fb99fb145a8
paul <paul.cowie@ntlworld.com>
2015-03-05 21:29:13 +0000

    Admin site/models almost functional Had to move models into one file due to circular import problems

--- bark/bark/models.py
+++ bark/bark/models.py
@@ -1,3 +1,129 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, unique=True)
+    followed_tags = models.ManyToManyField('Tag', through="TagFollowing")
+
+    user_tag = models.OneToOneField('UserTag', related_name="userprofile_user_tag", editable=False)
+    institution_tag = models.ForeignKey('InstitutionTag', related_name='userprofile_institution_tag', editable=False)
+
+    def canPostToTag(self, instTag):
+        # Checks if a user can post to an InstitutionTag
+        return instTag == self.institution_tag
+
+    def save(self, *args, **kwargs):
+        institution = getInstitution(self.user.email)
+        self.user_tag = UserTag.objects.create(name=self.user.username)
+        self.institution_tag = InstitutionTag.objects.get_or_create(name=institution)[0]
+        super(UserProfile, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.user.username
+
+
+class Comment(models.Model):
+    author = models.ForeignKey('UserProfile')
+    post = models.ForeignKey('Post')
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    content = models.TextField()
+
+    def __unicode__(self):
+        return self.content
+
+
+class Like(models.Model):
+    author = models.ForeignKey('UserProfile')
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.author + "," + self.creation_date
+
+
+class PostLike(Like):
+    post = models.ForeignKey('Post')
+
+    def __unicode__(self):
+        return self.post + "," + super(Like, self)
+
+
+class CommentLike(Like):
+    comment = models.ForeignKey('Comment')
+
+    def __unicode__(self):
+        return self.comment + "," + super(Like, self)
+
+
+class Post(models.Model):
+    author = models.ForeignKey('UserProfile')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    tags = models.ManyToManyField('Tag', through="PostTagging", null=False, blank=False)
+    rating = 0.0
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    slug = models.SlugField(editable=False, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.title
+
+
+class PostTagging(models.Model):
+    post = models.ForeignKey('Post')
+    tag = models.ForeignKey('Tag')
+    tagging_date = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(unique=True, max_length=30)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    followers = models.ManyToManyField('UserProfile', through="TagFollowing", null=True)
+    posts = models.ManyToManyField('Post', through="PostTagging", null=True)
+    description = models.TextField(blank=True)
+
+    def save(self, *args, **kwargs):
+        # Run clean()
+        self.full_clean()
+        self.genDescription()
+        super(Tag, self).save(*args, **kwargs)
+
+    def genDescription(self):
+        self.description = "This is for discussing " + self.name + "."
+
+    def clean(self):
+        # Remove any spaces from name
+        if self.name:
+            self.name.replace(" ", "")
+
+    def __unicode__(self):
+        return self.name
+
+
+class InstitutionTag(Tag):
+    def genDescription(self):
+        self.description = "Posting in this tag is restricted to " + self.name + " students."
+
+
+class UserTag(Tag):
+    def genDescription(self):
+        self.description = "This tag lists the posts of " + self.name + "."
+
+
+class TagFollowing(models.Model):
+    user = models.ForeignKey('UserProfile')
+    tag = models.ForeignKey('Tag')
+    follow_date = models.DateTimeField(auto_now_add=True)
+
+
+def getInstitution(email):
+    # Could be changed to return domain.ac.uk instead of *.ac.uk
+    return email.split("@")[1]
--- bark/bark/urls.py
+++ bark/bark/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from bark.views import views, regviews
+from views import views, regviews
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,76 +11,76 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
-    	#PROJECT URLS
-    	#----------
-
-		#Main Application Root
-        url(r'^$', views.index, name='index'),
-
-        #Just redirect back to homepage?
-        #TODO
-        url(r'^bark/' views.index, name='Barkindex'),
-
-		#BARK URLS
-		#----------
-
-        #Generic Post list
-        #TODO
-        #Add tags as /TAG/TAG/TAG to filter depending on the tags
-        url(r'^bark/tags/', views.barks, name='barks'),
-
-        #Specific Bark view
-        url(r'^bark/<post_id>/', views.barkview, name ='barkview'),
-       
-        #Add new Bark
-        url(r'^bark/new/', views.addbark, name='addbark'),
-
-        #Search
-        #Followed by query string or some form of search regex
-        url(r'^search/', views.search, name='search'),
-
-
-        #UUSER PROFILE FILES
-        #---------------
-
-        #Displays list of users
-        #Ranked by helpfulness?
-        #TODO
-        url(r'^users/', regviews.users, name='users'),
-
-        #Displays specific user profile
-        url(r'^users/<user_name>/', regviews.viewuser, name='viewuser'),
-
-        #PERSONAL PROFILE VIEWS
-        #------------
-
-        #View your own profile, when logged in
-        url(r'^profile/', regviews.profile, name="profile"),
-
-        #Update your profile information
-        url(r'^profile/update/', regviews.profileUpdate, name='profileUpdate'),
-
-
-        #REGISTRATION VIEWS
-        #---------
-
-        #Register
-        url(r'^signup/', regviews.signup, name='signup'),
-
-        #Login
-        url(r'^signin/', regviews.signin, name = 'signin'),
-
-        #Logout
-        url(r'^signout/', regviews.signout, name='signout'),
-
-        #Display password options
-        url(r'^password/', regviews.passwordMenu, name = 'passwordMenu'),
-
-        #Change password
-        url(r'^password/change/', regviews.passwordChange, name = 'passwordChange'),
-        
-        #Reset password
-        url(r'^password/reset/', regviews.passwordReset, name= 'passwordReset'),
+    # #PROJECT URLS
+    # #----------
+    #
+    # #Main Application Root
+    # url(r'^$', views.index, name='index'),
+    #
+    # #Just redirect back to homepage?
+    # #TODO
+    # url(r'^bark/', views.index, name='Barkindex'),
+    #
+    # #BARK URLS
+    # #----------
+    #
+    # #Generic Post list
+    # #TODO
+    # #Add tags as /TAG/TAG/TAG to filter depending on the tags
+    # url(r'^bark/tags/', views.barks, name='barks'),
+    #
+    # #Specific Bark view
+    # url(r'^bark/<post_id>/', views.barkview, name ='barkview'),
+    #
+    # #Add new Bark
+    # url(r'^bark/new/', views.addbark, name='addbark'),
+    #
+    # #Search
+    # #Followed by query string or some form of search regex
+    # url(r'^search/', views.search, name='search'),
+    #
+    #
+    # #UUSER PROFILE FILES
+    # #---------------
+    #
+    # #Displays list of users
+    # #Ranked by helpfulness?
+    # #TODO
+    # url(r'^users/', regviews.users, name='users'),
+    #
+    # #Displays specific user profile
+    # url(r'^users/<user_name>/', regviews.viewuser, name='viewuser'),
+    #
+    # #PERSONAL PROFILE VIEWS
+    # #------------
+    #
+    # #View your own profile, when logged in
+    # url(r'^profile/', regviews.profile, name="profile"),
+    #
+    # #Update your profile information
+    # url(r'^profile/update/', regviews.profileUpdate, name='profileUpdate'),
+    #
+    #
+    # #REGISTRATION VIEWS
+    # #---------
+    #
+    # #Register
+    # url(r'^signup/', regviews.signup, name='signup'),
+    #
+    # #Login
+    # url(r'^signin/', regviews.signin, name = 'signin'),
+    #
+    # #Logout
+    # url(r'^signout/', regviews.signout, name='signout'),
+    #
+    # #Display password options
+    # url(r'^password/', regviews.passwordMenu, name = 'passwordMenu'),
+    #
+    # #Change password
+    # url(r'^password/change/', regviews.passwordChange, name = 'passwordChange'),
+    #
+    # #Reset password
+    # url(r'^password/reset/', regviews.passwordReset, name= 'passwordReset'),
 
 )
 
@@ -91,6 +91,7 @@ if settings.DEBUG:
         'django.views.static',
         (r'^media/(?P<path>.*)',
         'serve',
-        {'document_root': settings.MEDIA_ROOT}), 
+        {'document_root': settings.MEDIA_ROOT}),
+        )
 
 
