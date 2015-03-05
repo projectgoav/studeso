18dcbbcd0ab4961b2a19a11f166f8a2a8a8ccf46
unknown <2093581M@BO706-09.ad.dcs.gla.ac.uk>
2015-03-05 10:49:21 +0000

    Stuff

--- bark/bark/urls.py
+++ bark/bark/urls.py
@@ -14,52 +14,25 @@ urlpatterns = patterns('',
     	#PROJECT URLS
     	#----------
 
-		#Main Application Root
+	#Main Application Root
         url(r'^$', views.index, name = 'index'),
-
-        #Just redirect back to homepage?
-        #TODO
-        url(r'^bark/', views.index, name = 'barkIndex'),
-
 		#BARK URLS
 		#----------
 
         #Generic Post list
         # TODO
         # Add tags as /TAG/TAG/TAG to filter depending on the tags
-        url(r'^bark/tags/', views.barks, name = 'barks'),
+        url(r'^tags/', views.barks, name = 'barks'),
 
         #Specific Bark view
-        url(r'^bark/<post_id>/', views.barkview, name = 'barkview'),
+        url(r'^<post_id>/', views.barkview, name = 'barkview'),
        
         #Add new Bark
-        url(r'^bark/new/', views.addbark, name = 'addbark'),
+        url(r'^new/', views.addbark, name = 'addbark'),
 
         #Search
         #Followed by query string or some form of search regex
         url(r'^search/', views.search, name = 'search'),
-
-
-        #UUSER PROFILE FILES
-        #---------------
-
-        #Displays list of users
-        #Ranked by helpfulness?
-        #TODO
-        url(r'^users/', regviews.users, name = 'users'),
-
-        #Displays specific user profile
-        url(r'^users/<user_name>/', regviews.viewuser, name = 'viewuser'),
-
-        #PERSONAL PROFILE VIEWS
-        #------------
-
-        #View your own profile, when logged in
-        url(r'^profile/', regviews.profile, name = "profile"),
-
-        #Update your profile information
-        url(r'^profile/update/', regviews.profileUpdate, name = 'profileUpdate'),
-
 
         #REGISTRATION VIEWS
         #---------
