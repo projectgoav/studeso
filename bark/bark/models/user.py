3e89f017f27fd5a7c643f44da8e49345620cc07c
2082442C <2082442C@student.gla.ac.uk>
2015-03-05 12:48:24 +0000

    Auto add user's UserTag and InstitutionTag on create

--- bark/bark/models/user.py
+++ bark/bark/models/user.py
@@ -1,3 +1,4 @@
+from bark.bark.models.tag import InstitutionTag
 from django.db import models
 from django.contrib.auth.models import User
 from bark.models.tag import Tag, UserTag, TagFollowing
@@ -7,11 +8,23 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, unique=True)
     followed_tags = models.ManyToManyField(Tag, through="TagFollowing")
 
+    user_tag = models.OneToOneField(UserTag)
+    institution_tag = models.ForeignKey(InstitutionTag)
 
-    # The tag created to link to the user
-    user_tag = models.ForeignKey(UserTag, unique=True, default=user.username, editable=False)
+    def canPostToTag(self, instTag):
+        # Checks if a user can post to an InstitutionTag
+        return instTag == self.institution_tag
 
-    # Other stuff to be added later...
+    def save(self, *args, **kwargs):
+        institution = getInstitution(self.user.email)
+        self.user_tag = UserTag.objects.create(name=self.user.username)
+        self.institution_tag = InstitutionTag.objects.get_or_create(name=institution)
+        super(Tag, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.user.username
+
+
+def getInstitution(email):
+    # Could be changed to return domain.ac.uk instead of *.ac.uk
+    return email.split("@")[1]
