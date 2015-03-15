b281b250b770aeb2d326faf0d95dc8c3fcef054f
Lewis Dicks <lewis_dicks@live.co.uk>
2015-03-15 13:04:11 +0000

    Button to like a post created
    Not tested but the code is there when we need to add it to the site

--- bark/static/js/ajax.js
+++ bark/static/js/ajax.js
@@ -9,3 +9,12 @@ $('#suggestion').keyup(function(){
          $('#tags').html(data);
         });
 });
+
+$('#rating').click(function(){
+    var Post_slug;
+    catid = $(this).attr("data-Post_slug");
+    $.get('/bark/like_post/', {Post_slug: Post_slug}, function(data){
+               $('#rating_count').html(data);
+               $('#rating').hide();
+    });
+});
