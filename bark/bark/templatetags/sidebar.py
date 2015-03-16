f0c1eae95e3ea09f63467879fb943d190727aa7e
projectgoav <2025797M@student.gla.ac.uk>
2015-03-16 20:18:30 +0000

    Started work on a basic template for the homepage. Removed my copy/pasted css to a new file and added a bark css. Added a sidebar that just displays all tags and colours them depending on type.

---
+++ bark/bark/templatetags/sidebar.py
@@ -0,0 +1,19 @@
+from django import template
+from bark.models import UserTag, InstitutionTag, Tag
+
+register = template.Library()
+
+@register.inclusion_tag('bark/sidebar.html')
+def get_sidebar():
+
+    user_tags = UserTag.objects.all()
+    inst_tags = InstitutionTag.objects.all()
+    other_tags = []
+
+    tags = Tag.objects.all()
+    for t in tags:
+
+        if t not in user_tags and t not in inst_tags:
+            other_tags += [t]
+
+    return {'U' : user_tags, 'I' : inst_tags, 'A' : other_tags }
