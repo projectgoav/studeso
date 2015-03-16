11914f1e2bd750d86a23d3a3f9bb73589ff6c420
SCOTPAUL <paul.cowie@ntlworld.com>
2015-03-16 21:10:22 +0000

    Bootstrap in add post

--- bark/bark/forms.py
+++ bark/bark/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from bark.models import UserProfile, Post
 from django.views.generic.edit import CreateView
 
+
 # Get data for user when they sign up
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -11,20 +12,19 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password')
 
+
 # Get data for the User Profile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('profile_picture',)
 
-class PostForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the new post.")
-    content = forms.CharField(max_length=1000, help_text="Please enter the post content.")
 
+class PostForm(forms.ModelForm):
     class Meta:
         # Provide an association between PostForm and Post.
         model = Post
-        exclude = ('slug', 'views', 'author', 'creation_date',)
+        exclude = ('slug', 'views', 'author', 'creation_date', 'rating',)
 
         def form_valid(self, form):
             form.instance.author = self.request.user
