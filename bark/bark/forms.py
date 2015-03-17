5e566a33a9bac81ca06d6fdd6773e48df866457b
projectgoav <2025797M@student.gla.ac.uk>
2015-03-17 11:47:46 +0000

    Signin form now uses forms and boostrap3 package.

--- bark/bark/forms.py
+++ bark/bark/forms.py
@@ -35,12 +35,17 @@ class LoginForm(forms.Form):
     username = forms.CharField(label='Username', max_length=50)
     password = forms.CharField(label='Password', max_length=50, widget=forms.PasswordInput())
 
-    # TODO -> FINISH THIS.
-
     def clean(self):
+
+        #Checking the existance of the username and password
+        cleaned_data = super(LoginForm, self).clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
         try:
-            user = User.objects.get(username=self.username)
-            if not request.user.check_password(oldpass):
-                break
+            user = User.objects.get(username=username)
         except:
-             raise forms.ValidationError("Given username and password is invalid.")
+            self.add_error('username', "Username not found")
+            return
+
+        if not user.check_password(password):
+            self.add_error('password', "Password invalid")
