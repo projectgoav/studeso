45d65ca41b32a1934150f98a47c93c7e9563e855
paul <paul.cowie@ntlworld.com>
2015-03-20 23:03:39 +0000

    Make images embedded using markdown responsive across site Bit of a hack, I might fix it later

---
+++ bark/static/js/bark.js
@@ -0,0 +1,9 @@
+/**
+ * Created by paul on 20/03/15.
+ */
+
+$(document).ready(function(){
+    if(!$('p > img').hasClass('img-responsive')){
+        $('p > img').addClass('img-responsive');
+    }
+});
