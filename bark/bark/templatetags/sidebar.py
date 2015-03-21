0b15d199abdf8457ac3975d23b03c1a9f763dd51
projectgoav <2025797M@student.gla.ac.uk>
2015-03-21 12:49:08 +0000

    Added some in-sidebar navigation. Sidebar will also display differently if the user is logged in or not. Doesn't sure tags they follow yet.

--- bark/bark/templatetags/sidebar.py
+++ bark/bark/templatetags/sidebar.py
@@ -4,8 +4,16 @@ from bark.models import UserTag, InstitutionTag, Tag
 register = template.Library()
 
 @register.inclusion_tag('sidebar.html')
-def get_sidebar():
+def get_sidebar(is_auth):
+
     tags = Tag.objects.all().exclude(id__in=UserTag.objects.all()).exclude(id__in=InstitutionTag.objects.all())
     users =  Tag.objects.all().filter(id__in=UserTag.objects.all())
     inst = Tag.objects.all().filter(id__in=InstitutionTag.objects.all())
+
+    if is_auth == True:
+        followed = [ ]
+
+        if (len(followed) == 0):
+            return { 'U' : users, 'I' : inst, 'A' : tags, 'F' : followed, 'FE' : { }}
+        return { 'U' : users, 'I' : inst, 'A' : tags, 'F' : followed}
     return {'U' : users, 'I' : inst, 'A' : tags }
